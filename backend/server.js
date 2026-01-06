cb41d44664b8d4946056c278f0b5fe4aee632301
epradhiksha <eprathiksha@gmail.com>
2026-01-06 16:04:27 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ connectDB();
 
 app.use("/api/books", bookRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
